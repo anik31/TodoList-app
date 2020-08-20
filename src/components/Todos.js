c241359603fa1b0e5b6b0a466010197be1eb5f33
Aniket <b218004@iiit-bh.ac.in>
2020-08-20 12:57:29 +0530

    Complete TodoList app

---
+++ src/components/Todos.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import Todoitems from "./Todoitems";
+import PropTypes from "prop-types";
+
+export class Todos extends Component {
+  render() {
+    return this.props.todos.map((todo) => (
+      <Todoitems
+        key={todo.id}
+        todo={todo}
+        markComplete={this.props.markComplete}
+        delTodo={this.props.delTodo}
+      />
+    ));
+  }
+}
+
+Todos.propTypes = {
+  todos: PropTypes.array.isRequired,
+  markComplete: PropTypes.func.isRequired,
+  delTodo: PropTypes.func.isRequired,
+};
+export default Todos;
